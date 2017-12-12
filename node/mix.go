b89fe36ffc1fe73838d9ef349396d8c4fe69202b
Ania Piotrowska <piotrowska@google.com>
2017-12-12 11:09:33 +0000

    Added delay of the packets by mix

--- node/mix.go
+++ node/mix.go
@@ -3,6 +3,7 @@ package anonymous_messaging
 import (
 	"fmt"
 	packet_format "anonymous-messaging/packet_format"
+	"time"
 )
 
 type Mix struct {
@@ -16,7 +17,14 @@ func (m Mix) ProcessPacket(p packet_format.Packet, c chan<- packet_format.Packet
 	fmt.Println("> Processing packet")
 
 	dePacket := p
-	c <- dePacket
+	timeoutCh := make(chan packet_format.Packet, 1)
+
+	go func(p packet_format.Packet, delay float64) {
+		time.Sleep(time.Second * time.Duration(delay))
+		timeoutCh <- p
+	}(dePacket, dePacket.Delays[0])
+
+	c <- <- timeoutCh
 }
 
 func (m Mix) SendLoopMessage() {
