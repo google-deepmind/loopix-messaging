bee307bce0ac1a66cd2b4a205183e94b331e5ae7
Ania Piotrowska <piotrowska@google.com>
2018-01-04 15:21:07 +0000

    Added temporary main file for server

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"anonymous-messaging/server"
+)
+
+func main() {
+	// args := os.Args
+
+	mixServer := server.NewMixServer("Mix","localhost", "9999", 0, 0, "./pki/database.db")
+	mixServer.Start()
+}
