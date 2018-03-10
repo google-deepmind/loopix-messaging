b563ad9285b9fb84915536229056423b6a3d6320
Ania Piotrowska <piotrowska@google.com>
2018-03-10 19:09:43 +0000

    Add function for extracting local ip

--- helpers/utilities.go
+++ helpers/utilities.go
@@ -156,3 +156,36 @@ func GetClientPKI(pkiDir string) ([]config.ClientConfig, error) {
 	}
 	return clients, nil
 }
+
+func GetLocalIP() (string, error) {
+	ifaces, err := net.Interfaces()
+	if err != nil {
+		return "", err
+	}
+
+	for _, iface := range ifaces {
+		addrs, err := iface.Addrs()
+		if err != nil {
+			return "", err
+		}
+		for _, addr := range addrs {
+			var ip net.IP
+			switch v := addr.(type) {
+			case *net.IPNet:
+				ip = v.IP
+			case *net.IPAddr:
+				ip = v.IP
+			}
+			if ip == nil || ip.IsLoopback() {
+				continue
+			}
+			ip = ip.To4()
+			if ip == nil {
+				continue // not an ipv4 address
+			}
+			return ip.String(), nil
+		}
+	}
+
+	return "", errors.New("Couldn't find the valid IP. Check internet connection.")
+}
